c30af7ea3ebd475b99d6526a4b6632b9608c79ac
zzaeun <leeje57575@gmail.com>
2025-08-17 00:57:23 +0900

    셀에 완료 체크, 즐겨찾기 버튼 추가

---
+++ TodoApp/TodoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TodoTableViewCell.swift
+//  TodoApp
+//
+//  Created by 이정은 on 8/16/25.
+//
+
+import UIKit
+
+class TodoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
