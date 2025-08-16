6acada5ae335acac3cb484f9061169e36e182385
zzaeun <leeje57575@gmail.com>
2025-08-16 20:57:27 +0900

    할 일 입력하고 리스트에 표시, 셀을 밀어서 삭제하거나 탭해서 수정 가능

---
+++ TodoApp/AddTodoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddTodoViewController.swift
+//  TodoApp
+//
+//  Created by 이정은 on 8/14/25.
+//
+
+import UIKit
+
+class AddTodoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
